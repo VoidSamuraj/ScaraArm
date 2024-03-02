2db648dee4acf87d88d5f80431843882e031d311
VoidSamuraj <karol.robak124@gmail.com>
2024-03-02 22:14:58 +0100

    rules

--- src/main/kotlin/com/voidsamuraj/Keys.kt
+++
@@ -1,8 +0,0 @@
-package com.voidsamuraj
-import io.ktor.util.*
-
-object Keys {
-    val EncryptKey = hex("YOUR_ENCRYPTION_KEY")
-    val SignKey = hex("YOUR_SIGN_KEY")
-    val JWTSecret = "YOUR_JWT_SECRET"
-}
